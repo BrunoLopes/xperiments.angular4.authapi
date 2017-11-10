47300925b79a97cbacf4819c9a5272cc653b17e6
Rodrigues Lopes <bole@gft.com>
2017-11-10 17:16:42 -0200

    Implementa jwt

--- server.js
+++ server.js
@@ -3,77 +3,72 @@ var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
+var User = require('./models/User')
+var auth = require('./auth')
+var Post = require('./models/Post')
 var jwt = require('jwt-simple')
 
-var User = require('./models/User')
-
-var posts = [
-    { message: 'hello' },
-    { message: 'hi' }
-]
+mongoose.Promise = Promise
 
 app.use(cors())
 app.use(bodyParser.json())
+
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
 });
 
-app.get('/posts', (req, res) => {
+app.get('/posts/:id', async (req, res) => {
+    var author = req.params.id
+    var posts = await Post.find({author})
     res.send(posts)
 });
 
-app.get('/users', async (req, res) => {
+app.post('/post', (req, res) => {
+    var postData = req.body
+    postData.author = '5a05aaa2c6c6b852d4be1e07'
 
-try
-{
-    var users = await User.find( {  }, '-pwd -__v' )
-    res.send(users)
-}
-catch(error)
-{
-    console.error(error)
-    res.send(500)
-}
-
-
-    
-});
-
-app.post('/register', (req, res) => {
-    var userData = req.body;
-
-    var user = new User(userData)
-
-    user.save((err, result) => {
-        if (err)
-            console.log('saving user error')
-
+    var post = new Post(postData)
+    console.log(post)
+    post.save((err, result) => {
+        if (err){
+            console.log('saving post error')
+            return res.status(500).send({message:'saving post error'})
+        }
         res.sendStatus(200)
-    });
-
+    })
 });
 
-app.post('/login', async (req, res) => {
-    var userData = req.body;
-
-    var user = await User.findOne({ email: userData.email })
-
-    if(!user)
-        return res.status(401).send({ message: 'Email or password invalid' })
-
-    if(userData.pwd != user.pwd)
-        return res.status(401).send({ message: 'Email or password invalid' })
-    
-    var payload = {}
-
-    var token = jwt.encode(payload, 'secret123')
-
-    console.log(token)
-
-    res.status(200).send({token: token})
-
+app.get('/users', auth.checkAuthenticated, async (req, res) => 
+{
+    try
+    {
+        var users = await User.find( {  }, '-pwd -__v' )
+        res.send(users)
+    }
+    catch(error)
+    {
+        console.error(error)
+        res.send(500)
+    }
 });
+
+app.get('/profile/:id', async (req, res) => 
+{
+    console.log(req.params.id)
+    try
+    {
+        var user = await User.findById( req.params.id, '-pwd -__v' )
+        res.send(user)
+    }
+    catch(error)
+    {
+        console.error(error)
+         res.send(500)
+    }
+});
+
 
 //mongoose.connect('mongodb://localhost:27017/xperiment-mongodb', { useMongoClient: true }, (err) => {
 mongoose.connect('mongodb://xperiment:xperiment@ds151955.mlab.com:51955/xperiment-mongodb', { useMongoClient: true }, (err) => {
@@ -82,5 +77,7 @@ mongoose.connect('mongodb://xperiment:xperiment@ds151955.mlab.com:51955/xperimen
     else
         console.log('Connected to mongodb')
 });
+
+app.use('/auth', auth.router)
 
 app.listen(6799)
